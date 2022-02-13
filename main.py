d482d189835278ad5c33538efa3fc9a68e0bd7b2
Larry Holland <larry.holland4779@hotmail.com>
2022-02-13 14:31:03 -0500

    Created function which displays background image

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import random
 
 WIDTH, HEIGHT = 750,750
+#Considered a pygame surface that we can draw on.
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Space Invader Twist")
 
@@ -23,16 +24,23 @@ BLUE_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_blue.png"))
 YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"))
 
 #Background
-BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
 def main():
     run = True
     FPS = 60
     clock = pygame.time.Clock()
 
+    def redraw_window():
+        WIN.blit(BG, (0,0))
+
+        #refreshes the display.
+        pygame.display.update()
+
     while run:
         #Tick the clock based on FPS. Allows consistency on multiple devices.
         clock.tick(FPS)
+        redraw_window()
 
         #Everytime we run the loop (60 times each second) loop through all events and
         #Check if an event occurs. If so do something.
