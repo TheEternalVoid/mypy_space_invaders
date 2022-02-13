cb64309cf02f5f008063b26e18c439529b7ac7cb
Larry Holland <larry.holland4779@hotmail.com>
2022-02-13 17:52:16 -0500

    Created Ship class and created player movement

--- main.py
+++ main.py
@@ -28,12 +28,34 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 #Background
 BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background-black.png")), (WIDTH, HEIGHT))
 
+#Generic Ship class to be inherited by both player and enemy ships
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    #Method to draw instance to window
+    def draw(self, window):
+        pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50), 0)
+
+
 def main():
     run = True
     FPS = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont("comicsans", 50)
+    
+    #Ship velocity
+    player_vel = 5
+
+    #Instanciated instance of Ship class
+    ship = Ship(300, 650)
 
     clock = pygame.time.Clock()
 
@@ -45,8 +67,12 @@ def main():
         lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
         level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
 
+        #Using non-hard coded values allows for window resizing while maintaining render placements
         WIN.blit(lives_label, (10,10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        #Method used to draw instance of ship to window
+        ship.draw(WIN)
 
         #Refreshes the display.
         pygame.display.update()
@@ -62,5 +88,17 @@ def main():
             #If you click the X in the pygame window run equals False
             if event.type == pygame.QUIT:
                 run = False
+        
+        #Checks based on fps what keys are pressed and returns bool values with True if they are pressed
+        keys = pygame.key.get_pressed()
+        #If statement for the key at this index (.K represents keyboard input and _value equals which key)
+        if keys[pygame.K_a]: #left
+            ship.x -= player_vel
+        if keys[pygame.K_d]: #right
+            ship.x += player_vel
+        if keys[pygame.K_w]: #up
+            ship.y -= player_vel
+        if keys[pygame.K_s]: #down
+            ship.y += player_vel
 
 main()
