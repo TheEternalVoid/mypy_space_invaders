31e271afac60f516ed4457a85d6bfd0bdf809f76
Larry Holland <larry.holland4779@hotmail.com>
2022-02-13 17:26:56 -0500

    Created Level and Lives UI and Rendered to Screen

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
+
 
 WIDTH, HEIGHT = 750,750
 #Considered a pygame surface that we can draw on.
@@ -29,12 +31,24 @@ BG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "background
 def main():
     run = True
     FPS = 60
+    level = 1
+    lives = 5
+    main_font = pygame.font.SysFont("comicsans", 50)
+
     clock = pygame.time.Clock()
 
     def redraw_window():
+        #Draw background to window
         WIN.blit(BG, (0,0))
 
-        #refreshes the display.
+        #Draw Text to window
+        lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+        level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+        WIN.blit(lives_label, (10,10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        #Refreshes the display.
         pygame.display.update()
 
     while run:
